89c2e2295fd30874a5bc24dfb71f322b5246595a
Farouk Abdi <abdifarouk58@gmail.com>
2025-03-21 09:00:10 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+// beforeEach(function () {
+//     cats.length = 0;
+
+//     cats.push('Milo', 'Otis', 'Garfield');
+//   });
+
+function destructivelyAppendCat(name){
+    return cats.push(`${name}`);
+}
+// console.log(destructivelyAppendCat("Ralph"));
+
+function destructivelyPrependCat(name){
+    return cats.unshift(`${name}`)
+}
+// console.log(destructivelyPrependCat("Bob"));
+
+function destructivelyRemoveLastCat(name){
+    return cats.pop(`${name}`)
+}
+// console.log(destructivelyRemoveLastCat("Ralph"));
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(`${name}`)
+}
+// console.log(destructivelyRemoveFirstCat("Bob"));
+
+function appendCat(){
+    let catCool;
+    return catCool = [...cats, "Broom"]
+}
+
+function prependCat(){
+    let prepCat;
+    return prepCat = ["Arnold", ...cats]
+}
+
+function removeLastCat(){
+    let remCat = cats.slice();
+    remCat.pop();
+    return remCat
+}
+//   console.log(removeLastCat())
+
+function removeFirstCat(){
+    let firstCat =cats.slice();
+    firstCat.shift();
+    return firstCat;
+}
+// console.log(removeFirstCat())
